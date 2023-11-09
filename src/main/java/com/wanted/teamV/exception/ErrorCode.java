97190ce269c879c048051a7c4c56036a117b2cd5
shinmin <shinmin9812@naver.com>
2023-11-09 19:01:37 +0900

    feat #3 - 예외처리 기능 구현 - 개발 중 에러처리 필요 시 ErrorCode에 추가 - ErrorResponse, CustomExcepiton, GlobalExceptionHandler 구현

---
+++ src/main/java/com/wanted/teamV/exception/ErrorCode.java
@@ -0,0 +1,31 @@
+package com.wanted.teamV.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다."),
+    DUPLICATE_ACCOUNT(HttpStatus.BAD_REQUEST, "중복된 계정입니다."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다."),
+    EXPIRE_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+    EMPTY_AUTHORIZATION_HEADER(HttpStatus.BAD_REQUEST, "인증헤더가 비어있습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(final HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/wanted/teamV/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.wanted.teamV.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String message;
+}
