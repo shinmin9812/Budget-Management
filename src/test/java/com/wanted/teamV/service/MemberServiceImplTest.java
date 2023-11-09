1d876c53960273863af52382cecdb119e7caba4e
shinmin <shinmin9812@naver.com>
2023-11-09 18:30:50 +0900

    test #1 - 사용자 회원가입, 로그인 테스트 코드 구현 - 사용자 회원가입, 로그인 테스트 코드 구현 - 예외처리 적용해서 수정 예정

---
+++ src/test/java/com/wanted/teamV/service/MemberServiceImplTest.java
@@ -0,0 +1,108 @@
+package com.wanted.teamV.service;
+
+import com.wanted.teamV.component.AuthTokenCreator;
+import com.wanted.teamV.dto.req.MemberJoinReqDto;
+import com.wanted.teamV.dto.req.MemberLoginReqDto;
+import com.wanted.teamV.dto.res.MemberTokenResDto;
+import com.wanted.teamV.entity.Member;
+import com.wanted.teamV.repository.MemberRepository;
+import com.wanted.teamV.service.impl.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceImplTest {
+
+    @InjectMocks
+    private MemberServiceImpl memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private AuthTokenCreator authTokenCreator;
+
+    @Test
+    @DisplayName("회원가입 - 성공")
+    @Transactional
+    public void join() throws Exception {
+        //given
+        MemberJoinReqDto request = new MemberJoinReqDto("test1234", "test1234!@#$");
+        when(memberRepository.existsByAccount(anyString())).thenReturn(false);
+        when(passwordEncoder.encode(anyString())).thenReturn("encryptedPassword");
+
+        //when
+        ResponseEntity<Void> response = memberService.join(request);
+
+        //then
+        verify(memberRepository, times(1)).save(any(Member.class));
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("회원가입 - 중복 계정")
+    @Transactional
+    public void join_duplicate_account() throws Exception {
+        //given
+        MemberJoinReqDto request = new MemberJoinReqDto("test1234", "test1234!@#$");
+        when(memberRepository.existsByAccount(anyString())).thenReturn(true);
+
+        //when & then
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> memberService.join(request));
+        assertEquals(HttpStatus.BAD_REQUEST, runtimeException.getStackTrace());
+    }
+
+    @Test
+    @DisplayName("로그인 - 성공")
+    @Transactional
+    public void login() throws Exception {
+        //given
+        Member member = new Member("test1234", "hashedPassword");
+
+        when(memberRepository.getByAccount("test1234")).thenReturn(member);
+        when(passwordEncoder.matches("test1234!@#$", "hashedPassword")).thenReturn(true);
+        when(authTokenCreator.createAuthToken(member.getId())).thenReturn("qwerqrwqrr.12323131.assdas");
+
+        //when
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "test1234!@#$");
+        MemberTokenResDto response = memberService.login(request);
+
+        //then
+        assertNotNull(response.accessToken());
+
+    }
+
+    @Test
+    @DisplayName("로그인 - 실패(잘못된 비밀번호)")
+    @Transactional
+    public void login_invalid_password() throws Exception {
+        //given
+        Member member = new Member("test1234", "hashedPassword");
+
+        when(memberRepository.getByAccount("test1234")).thenReturn(member);
+        when(passwordEncoder.matches("wrongPassword", "hashedPassword")).thenReturn(false);
+
+        //when
+        MemberLoginReqDto request = new MemberLoginReqDto("test1234", "wrongPassword");
+
+        //then
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () -> memberService.login(request));
+        assertEquals(HttpStatus.BAD_REQUEST, runtimeException.getStackTrace());
+    }
+}
